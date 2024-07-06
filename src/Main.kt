0c3ed6c9dd67cc13002c39b9a90a0478ab0f6c6b
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-07-06 15:18:01 -0300

    solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,39 @@
+import kotlin.random.Random
+
+fun birthdayCakeCandles(candles: Array<Int>): Int {
+    if (candles.isEmpty()) return 0
+
+    var bigCandle = candles.first()
+    var count = 0
+
+    for (candle in candles) {
+        if (candle > bigCandle) {
+            bigCandle = candle
+            count = 1
+        } else if (candle == bigCandle) {
+            count++
+        }
+    }
+
+    return count
+}
+
+
 fun main() {
-    println("Hello World!")
+//    val candles = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val candles = randomCandleGenerate().trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(candles)
+
+    println(result)
+}
+
+
+fun randomCandleGenerate(): String {
+    val size = Random.nextInt(1, 10)
+    var candles = ""
+    for (i in 0..size) {
+        candles += "${Random.nextInt(1, 5)} "
+    }
+    return candles
 }
